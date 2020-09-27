3f1d52b29040459ca7735dbfc3a691abed06d3e2
Barbabosco <andrea.magnabosco@antartika.it>
2020-09-27 21:18:20 +0200

    added audio play

--- src/App.js
+++ src/App.js
@@ -4,13 +4,57 @@ function App() {
   const [minNumIncluded, setMinNumIncluded] = React.useState(0);
   const [maxNumIncluded, setMaxNumIncluded] = React.useState(99);
   // const  [quantity, setQuantity] = React.useState(4);
-  const [delay, setDelay] = React.useState(30);
+  const [delay, setDelay] = React.useState(20);
   const [isRunning, setIsRunning] = React.useState(false);
   const [counter, setCounter] = React.useState(0);
   const [colorIndex, setColorIndex] = React.useState(0);
   const [sequence, setSequence] = React.useState([]);
   const [sequenceLimit, setSequenceLimit] = React.useState(10);
   const [showSequence, setShowSequence] = React.useState(false);
+  const [playAudio, setPlayAudio] = React.useState(true);
+  const [showNumbers, setShowNumbers] = React.useState(true);
+
+  const audio0 = new Audio("/num0.mp3");
+  const audio1 = new Audio("/num1.mp3");
+  const audio2 = new Audio("/num2.mp3");
+  const audio3 = new Audio("/num3.mp3");
+  const audio4 = new Audio("/num4.mp3");
+  const audio5 = new Audio("/num5.mp3");
+  const audio6 = new Audio("/num6.mp3");
+  const audio7 = new Audio("/num7.mp3");
+  const audio8 = new Audio("/num8.mp3");
+  const audio9 = new Audio("/num9.mp3");
+
+  const playNum0 = () => {
+    audio0.play();
+  };
+  const playNum1 = () => {
+    audio1.play();
+  };
+  const playNum2 = () => {
+    audio2.play();
+  };
+  const playNum3 = () => {
+    audio3.play();
+  };
+  const playNum4 = () => {
+    audio4.play();
+  };
+  const playNum5 = () => {
+    audio5.play();
+  };
+  const playNum6 = () => {
+    audio6.play();
+  };
+  const playNum7 = () => {
+    audio7.play();
+  };
+  const playNum8 = () => {
+    audio8.play();
+  };
+  const playNum9 = () => {
+    audio9.play();
+  };
 
   function useInterval(callback, delay) {
     const savedCallback = React.useRef();
@@ -48,7 +92,43 @@ function App() {
       const lastRandomNum =
         Math.floor(Math.random() * (maxNumIncluded + 1 - minNumIncluded)) +
         minNumIncluded;
-
+      if (0 <= lastRandomNum && lastRandomNum <= 9) {
+        // if (lastRandomNum) {
+        //   playNum0();
+        // }
+        switch (lastRandomNum) {
+          case 0:
+            playNum0();
+            break;
+          case 1:
+            playNum1();
+            break;
+          case 2:
+            playNum2();
+            break;
+          case 3:
+            playNum3();
+            break;
+          case 4:
+            playNum4();
+            break;
+          case 5:
+            playNum5();
+            break;
+          case 6:
+            playNum6();
+            break;
+          case 7:
+            playNum7();
+            break;
+          case 8:
+            playNum8();
+            break;
+          case 9:
+            playNum9();
+            break;
+        }
+      }
       setSequence([...sequence, lastRandomNum]);
     },
     isRunning ? delay * 100 : null
@@ -85,13 +165,21 @@ function App() {
     setShowSequence(true);
   };
 
+  const handlePlayAudio = () => {
+    setPlayAudio(!playAudio);
+  };
+
+  const handleShowNumbers = () => {
+    setShowNumbers(!showNumbers);
+  };
+
   return (
     <div className="App">
       <header>
         <h1>Spoken words</h1>
+        <button onClick={playNum0}>prova</button>
       </header>
       <section>
-        <h3>Settings</h3>
         <div style={{ margin: "0.75rem 0rem" }}>
           Delay (deciseconds)&nbsp;
           <button
@@ -226,7 +314,6 @@ function App() {
             50
           </button>
         </div>
-
         <div style={{ margin: "0.75rem 0rem" }}>
           Sequence length &nbsp;
           <button
@@ -349,7 +436,6 @@ function App() {
             100
           </button>
         </div>
-
         <div style={{ margin: "0.75rem 0rem" }}>
           Min number &nbsp;
           <input
@@ -392,6 +478,20 @@ function App() {
             99
           </button>
         </div>
+        <div>
+          Play audio (only 0-9)
+          <input
+            type="checkbox"
+            checked={playAudio}
+            onChange={handlePlayAudio}
+          />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Show numbers
+          <input
+            type="checkbox"
+            checked={showNumbers}
+            onChange={handleShowNumbers}
+          />
+        </div>
       </section>
       <section>
         <div>
@@ -406,7 +506,11 @@ function App() {
       </section>
       <section>
         <div>
-          <Display sequence={sequence} colorIndex={colorIndex} />
+          <Display
+            sequence={sequence}
+            showNumbers={showNumbers}
+            colorIndex={colorIndex}
+          />
         </div>
         <div style={{ textAlign: "right" }}>
           <p>{isRunning ? `${sequence.length} / ${sequenceLimit}` : null}</p>
@@ -444,7 +548,7 @@ function App() {
 
 export default App;
 
-function Display({ sequence, colorIndex }) {
+function Display({ sequence, colorIndex, showNumbers }) {
   let colors = [
     "brown",
     "darkblue",
@@ -460,8 +564,21 @@ function Display({ sequence, colorIndex }) {
 
   return (
     <React.Fragment>
-      <h2 style={{ fontSize: "100px", color: colors[colorIndex] }}>
-        {sequence.length === 0 ? "--" : sequence[sequence.length - 1]}
+      <h2
+        style={{
+          fontSize: "100px",
+          color: colors[colorIndex],
+          backgroundColor: "#dddddd",
+          padding: "10px 20px",
+          textAlign: "center",
+          width: "150px",
+        }}
+      >
+        {sequence.length === 0 || !showNumbers ? (
+          <span style={{ color: "#000" }}>--</span>
+        ) : (
+          sequence[sequence.length - 1]
+        )}
       </h2>
     </React.Fragment>
   );
